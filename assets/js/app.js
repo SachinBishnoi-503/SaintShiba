90478c54910229b709f7304cd7f88687a3e38a45
SachinBishnoi-503 <bishnoisachin626@gmail.com>
2024-05-28 11:09:13 +0530

    all update solved

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,7 @@
 let menuicon = document.querySelector(".menubar");
 let menulist = document.querySelector(".navbar-iteams");
+var body = document.querySelector(".body");
 menuicon.addEventListener("click", function () {
     menulist.classList.toggle("show");
+    body.classList.toggle("overflow-lg-hidden")
 })
